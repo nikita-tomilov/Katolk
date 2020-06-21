65d2da0a2422aa861083a039b0db2e6a2ca85424
paekva <paekva@yandex.ru>
2020-06-21 18:07:08 +0300

    login page update

--- katolk-web-v2/src/components/LoginPage.tsx
+++ katolk-web-v2/src/components/LoginPage.tsx
@@ -1,6 +1,6 @@
-import React, {useCallback} from "react";
-import {DefaultButton} from "./basic/Buttons";
+import React, {useCallback, useState} from "react";
 import './LoginPage.css';
+import {Button, TextField} from "@material-ui/core";
 
 export type LoginPageProps = {
     setToken: (token: string) => void;
@@ -8,16 +8,72 @@ export type LoginPageProps = {
 
 export const LoginPage = (props: LoginPageProps): JSX.Element => {
     const { setToken } = props;
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [authFailMessage, setAuthFailMessage] = useState<string | null>(null);
+    const onNameChange = useCallback((event) => {
+        if (event.currentTarget && event.currentTarget.value)
+            setName(event.currentTarget.value);
+    }, []);
 
-    const clickHandler = useCallback(() => {
-        setToken('true');
-    }, [setToken]);
+    const onPasswordChange = useCallback((event) => {
+        if (event.currentTarget && event.currentTarget.value)
+            setPassword(event.currentTarget.value);
+    }, []);
+
+    const onSignIn = useCallback(() => {
+        setAuthFailMessage('FAILED');
+    }, [name, password, setToken]);
+
+    const onKeyPress = useCallback(
+        (e: React.KeyboardEvent) => {
+            if (e.which === 13) {
+                onSignIn();
+            }
+        },
+        [onSignIn]
+    );
 
     return (
-        <div className="loginPageWrapper">
-            <div className="btnContainer">
-                <DefaultButton onClick={clickHandler} title={'Login'} />
+        <div className="SignInForm">
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="login"
+                    label="Login"
+                    name="login"
+                    autoComplete="login"
+                    autoFocus
+                    value={name}
+                    onChange={onNameChange}
+                    onKeyPress={onKeyPress}
+                />
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={onPasswordChange}
+                    onKeyDown={onKeyPress}
+                />
+                {authFailMessage && <div className="error">{authFailMessage}</div>}
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="secondary"
+                    onClick={onSignIn}
+                >
+                    Sign In
+                </Button>
             </div>
-        </div>
     )
 }
--- katolk-web-v2/src/components/basic/Buttons.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './Buttons.css';
-
-export type DefaultButtonProps = React.HTMLAttributes<HTMLButtonElement>;
-
-export const DefaultButton = (props: DefaultButtonProps): JSX.Element => {
-    return <button {...props} className="btnWrapper" >{props.title}</button>;
-}
